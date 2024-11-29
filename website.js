63247e0ce01667da74bf03e33afb9a542cc9c09b
RoanneChristineMendoza <roanneamendoza@gmail.com>
2024-11-29 23:26:44 +0800

    Add files via upload

---
+++ website.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    const slides = document.querySelectorAll(".slide");
+    const dots = document.querySelectorAll(".dot");
+    
+    slides.forEach(slide => (slide.style.display = "none"));
+    dots.forEach(dot => dot.classList.remove("active"));
+
+    slideIndex++;
+    if (slideIndex > slides.length) slideIndex = 1;
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
+
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+function currentSlide(index) {
+    slideIndex = index;
+    showSlides();
+}
